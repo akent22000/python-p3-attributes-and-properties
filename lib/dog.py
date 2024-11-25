01703d1213e1fc927eb2f77c303a2b3e2c3aeb0b
amandalocher <akent22000@gmail.com>
2024-11-25 12:28:50 -0500

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+
+# Define a name property for your Dog class. The name must be of type str and between 1 and 25 characters. Your __init__ method should receive a default argument for name.
+# If the name is invalid, the setter method should print() "Name must be string between 1 and 25 characters."
+
+
+#!/usr/bin/env python3
+
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +19,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
